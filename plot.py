2858dde61bc6a4fe223c3e4784b45aa57242d6b9
Angad Kumar <angad9745@gmail.com>
2022-06-22 22:43:20 +0530

    update

--- plot.py
+++ plot.py
@@ -1,13 +1,17 @@
+from turtle import color
 import pandas as pd
 import matplotlib.pyplot as plt
 
-var = pd.read_excel("data_recorder.xlsx")
+var = pd.read_excel("data_recorder_ntpc_21_06.xlsx")
 # print(var)
 
 
 x = list(var['time'])
 y = list(var['buy_price'])
 
+# x.title("time")
 plt.plot(y)
+plt.xlabel("Time", color = "Red")
+plt.ylabel("Price", color = "Red")
 
 plt.show()
